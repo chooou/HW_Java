f74f4abe3024b6fdc43a55406342ee7f3729b4dc
chooou <irenechou91@gmail.com>
2022-10-27 09:50:43 +0800

    hw

---
+++ lab1/src/lab1/Lab1_2.java
@@ -0,0 +1,10 @@
+package lab1;
+//109360139 p.10
+public class Lab1_2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("歡迎使用Java");
+		System.out.println("開始使用Java吧!");
+	}
+}
---
+++ lab1/src/lab1/Lab1_7.java
@@ -0,0 +1,14 @@
+package lab1;
+//109360139 p.27
+public class Lab1_7 
+{
+	public static void main(String[] args) 
+	{
+		int num1, num2;
+		num1 = 3;
+		System.out.println("變數num1的值是:"+num1);
+		num2 = num1;
+		System.out.println("將變數num1指定到變數num2之中");
+		System.out.println("變數num2的值是:"+num2);
+	}
+}
